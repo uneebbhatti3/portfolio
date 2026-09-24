80f3c922f2b27db93829fd1ddd051e764015f8f5
UneebBhatti <uneebbhatti3@gmail.com>
2026-09-24 18:54:36 +0500

    clarity script updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -125,6 +125,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const clarityId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID;
+
   return (
     <html
       lang="en"
@@ -148,15 +150,17 @@ export default function RootLayout({
         {children}
         <Analytics />
         <SpeedInsights />
-        <Script id="clarity-script" strategy="afterInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
-          `}
-        </Script>
+        {clarityId && (
+          <Script id="microsoft-clarity" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://clarity.ms"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${clarityId}");
+            `}
+          </Script>
+        )}
       </body>
     </html>
   );
